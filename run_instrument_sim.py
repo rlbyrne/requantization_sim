307d717ba84f5bdfa761d3169004a06afbfeddfe
Ruby Byrne <rlbyrne@uw.edu>
2026-01-08 16:29:44 -0700

    more simulation code

--- run_instrument_sim.py
+++ run_instrument_sim.py
@@ -45,7 +45,8 @@ def constant_slope_sims():
 
     nfreqs = 2048
     average_signal_stddev = 16
-    frac_variation = np.array([0.01, 0.1, 0.5, 1])
+    # frac_variation = np.array([0.01, 0.1, 0.5, 1])
+    frac_variation = np.array([5])
     requantization_gain = 2**16
     target_value = 3 * requantization_gain
 
@@ -72,5 +73,78 @@ def constant_slope_sims():
         f.close()
 
 
+def increased_signal_simulations():
+
+    nfreqs = 2048
+    average_signal_stddev_array = np.array([50.0, 100.0, 500.0, 1000.0])
+    use_var = 1.0
+    requantization_gain = 2**16
+    target_value = 3 * requantization_gain
+
+    for average_signal_stddev in average_signal_stddev_array:
+        min_signal_stddev = 2 * average_signal_stddev / (2 + use_var)
+        max_signal_stddev = 2 * average_signal_stddev - min_signal_stddev
+        data_stddev = np.linspace(min_signal_stddev, max_signal_stddev, num=nfreqs)
+        eq_coeffs = target_value / data_stddev
+
+        final_variances, final_autocorrs = simulation_scripts.requantization_sim(
+            data_stddev,
+            eq_coeffs,
+            input_bits_total=18,
+            input_bits_fractional=0,
+            eq_coeff_bits_total=14,
+            eq_coeff_bits_fractional=0,
+            output_bits_total=4,
+            output_bits_fractional=0,
+            requantization_gain=requantization_gain,
+        )  # Run simulation
+        f = open(
+            f"const_slope_simulation_output_slope{use_var}_avg_signal{average_signal_stddev}.npy",
+            "wb",
+        )
+        np.save(f, final_variances)
+        np.save(f, final_autocorrs)
+        f.close()
+
+
+def increased_bit_depth_simulations():
+
+    nfreqs = 2048
+    average_signal_stddev = 16
+    output_bits_array = np.array([5, 6, 7, 8])
+    use_var = 1.0
+
+    for output_bits in output_bits_array:
+
+        requantization_gain = 2 ** (12 + output_bits)
+        target_value = 3 * requantization_gain
+
+        min_signal_stddev = 2 * average_signal_stddev / (2 + use_var)
+        max_signal_stddev = 2 * average_signal_stddev - min_signal_stddev
+        data_stddev = np.linspace(min_signal_stddev, max_signal_stddev, num=nfreqs)
+        eq_coeffs = target_value / data_stddev
+
+        final_variances, final_autocorrs = simulation_scripts.requantization_sim(
+            data_stddev,
+            eq_coeffs,
+            input_bits_total=18,
+            input_bits_fractional=0,
+            eq_coeff_bits_total=14,
+            eq_coeff_bits_fractional=0,
+            output_bits_total=output_bits,
+            output_bits_fractional=0,
+            requantization_gain=requantization_gain,
+        )  # Run simulation
+        f = open(
+            f"const_slope_simulation_output_slope{use_var}_bit_depth{output_bits}.npy",
+            "wb",
+        )
+        np.save(f, final_variances)
+        np.save(f, final_autocorrs)
+        f.close()
+
+
 if __name__ == "__main__":
     constant_slope_sims()
+    increased_signal_simulations()
+    increased_bit_depth_simulations()
